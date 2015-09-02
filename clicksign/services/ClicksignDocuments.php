54ba6c0d5db8ca801edc8ebd2384d48c187e9bea
Mauricio Vieira <mauricio@mauriciovieira.net>
2015-09-02 14:33:32 -0300

    Trim spaces

--- clicksign/services/ClicksignDocuments.php
+++ clicksign/services/ClicksignDocuments.php
@@ -1,10 +1,9 @@
 <?php
 require_once(__DIR__ . '/../ClicksignClient.php');
 require_once(__DIR__ . '/../ClicksignService.php');
-    
+
 class ClicksignDocuments extends ClicksignService
 {
-
     public function all()
     {
         $data = array();
@@ -18,10 +17,10 @@ class ClicksignDocuments extends ClicksignService
     }
 
     public function upload($filePath)
-	{
+    {
         $data = array("document[archive][original]" => "@$filePath");
         return $this->client->request("/documents", "FILE", $data, 200, "multipart/mixed; boundary=frontier");
-	}
+    }
 
     public function download($documentKey)
     {
@@ -34,5 +33,4 @@ class ClicksignDocuments extends ClicksignService
         $json = json_encode($data);
         return $this->client->request("/documents/$documentKey/list", "POST", $json, 200, "application/json");
     }
-    
 }
